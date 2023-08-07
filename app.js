a42a05334758cf94a8be91c900c0067786508208
Mhlael <mhlaelsultan74@gmail.com>
2023-08-06 21:39:53 -0500

    Preaper Files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'assets')))
+app.set('view engine', 'ejs')
+app.set('views', 'views') // default
+
+app.get('/',(req,res,next)=>{
+    res.send('Hello')
+})
+
+
+app.listen(3000,()=>{
+    console.log('server on port 3000')
+})
